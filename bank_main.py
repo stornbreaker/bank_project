fa30755c498bee482534b62016d9344d6f48cc8d
stornbreaker <cordonnier.antoine@yahoo.fr>
2021-04-21 13:10:54 +0200

    ajout premieres pages html

--- bank_main.py
+++ bank_main.py
@@ -2,11 +2,16 @@ import os
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 
-
+app = Flask(__name__)
 db = SQLAlchemy(app)
 
 @app.route("/", methods = ["GET","POST"])
 def log_in():
-    if request.form:
-        try:
-            
+    return render_template("log_in.html")
+
+@app.route("/log_in_client", methods =["GET","POST"] )
+def log_in_client():
+    return render_template("log_in_client.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
