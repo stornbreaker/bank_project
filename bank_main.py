bc216c92bf46f8b0b9dae1d39c44dd92f8b1b112
stornbreaker <cordonnier.antoine@yahoo.fr>
2021-04-21 16:28:10 +0200

    fix some pb in linking pages

--- bank_main.py
+++ bank_main.py
@@ -5,6 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+
+
+database_file = "sqlite:///bank.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
 @app.route("/", methods = ["GET","POST"])
 def log_in():
     return render_template("log_in.html")
@@ -15,7 +21,16 @@ def log_in_client():
 
 @app.route("/log_in_employee", methods =["GET","POST"] )
 def log_in_employee():
+    print(1234)
     return render_template("log_in_employee.html")
+
+@app.route("/menu_employee", methods =["POST"] )
+def menu_employee():
+    
+    employee_PIN=request.form.get("employee_PIN")
+    print(employee_PIN)
+    return render_template("menu_employee.html")
+
 
 
 if __name__ == "__main__":
