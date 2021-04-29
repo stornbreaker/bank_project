2a7d4a3ed9eb2900b1d146afb34ac131f82c0f52
unknown <cordonnier.antoine@yahoo.fr>
2021-04-29 19:38:09 +0200

    end of the day

--- bank_main.py
+++ bank_main.py
@@ -11,6 +11,8 @@ class customer(db.Model):
     accountNumber = db.Column(db.String,nullable=False,primary_key=True)
     firstName = db.Column(db.String,nullable= False)
     lastName = db.Column(db.String,nullable=False)
+    #currentAccount = db.Column(db.Integer, nullable=False)
+    #savingAccount = db.Column(db.Integer, nullable=False)
 
     def __init__(self,firstName,lastName,accountNumber):
         
@@ -22,6 +24,22 @@ class customer(db.Model):
 
     def __repr__(self):
         return '<customer %s>' % self.accountNumber
+
+    def getPIN(self):
+        return self.accountNumber[-5:-3] + self.accountNumber[-2:]
+
+    def getNum(self):
+        alphabet="abcdefghijklmonpqrstuvwxyz"
+        nombre = range(1,27)
+        initials=self.firstName[0].lower() + self.lastName[0].lower()
+
+        length = len(self.firstName) + len(self.lastName)
+        ### find the numbers corresponding
+        yy = nombre[alphabet.index(self.firstName[0].lower())]
+        zz = nombre[alphabet.index(self.lastName[0].lower())]
+
+        return initials + "-"+str(length) + "-" + "%02d-%02d" % (yy,zz)
+
 
 
 database_file = "sqlite:///bank.db"
@@ -57,6 +75,8 @@ def newCustomer():
     if request.method == 'POST':
         firstName = request.form['customer_firstName']
         lastName = request.form['customer_lastName']
+        
+
         accountNum = request.form['customer_accountNumber']
         newCos=customer(firstName=firstName,lastName=lastName,accountNumber= accountNum)
 
@@ -79,7 +99,7 @@ def new_customerPage():
 
 @app.route("/delete/<string:accNum>",methods = ['GET','POST'])
 def delete(accNum):
-    account_to_delete = customer.query.get_or_404('rys ')
+    account_to_delete = customer.query.get_or_404(accNum)
 
     try:
         db.session.delete(account_to_delete)
@@ -88,6 +108,24 @@ def delete(accNum):
         return render_template("menu_employee.html",accounts= accounts)
     except:
         return "there was a issue"
+
+@app.route("/menu_customer",methods = ['GET','POST'])
+def menuCustomer():
+    lastName = request.form['customer_lastName']
+    accountNum = request.form['customer_accountNumber']
+    PIN = request.form['customer_PIN']
+    print('a')
+    account_customer = customer.query.get_or_404(accountNum)
+    print('b')
+    if lastName == account_customer.lastName and PIN == account_customer.getPIN():
+        print("le log in est bon")
+        return render_template("/menu_customer.html",customer = account_customer)
+    return render_template("log_in_customer.html",message = "not fount try again")
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
