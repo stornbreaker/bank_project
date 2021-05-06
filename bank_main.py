21afd833b0bf906a8bd9f47bfc9c327ce1a30b61
unknown <cordonnier.antoine@yahoo.fr>
2021-05-06 21:05:41 +0200

    text files

--- bank_main.py
+++ bank_main.py
@@ -20,12 +20,14 @@ class customer(db.Model):
     lastName = db.Column(db.String,nullable=False)
     currentAccount = db.Column(db.Integer, nullable=False)
     savingAccount = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String,nullable=False)
 
-    def __init__(self,firstName,lastName,accountNumber):
+    def __init__(self,firstName,lastName,accountNumber,email):
         
         self.firstName = firstName
         self.lastName = lastName
         self.accountNumber = accountNumber
+        self.email = email
         self.currentAccount = 0
         self.savingAccount = 0
 
@@ -36,23 +38,13 @@ class customer(db.Model):
     def getPIN(self):
         return self.accountNumber[-5:-3] + self.accountNumber[-2:]
 
-    def getNum(self):
-        alphabet="abcdefghijklmonpqrstuvwxyz"
-        nombre = range(1,27)
-        initials=self.firstName[0].lower() + self.lastName[0].lower()
-
-        length = len(self.firstName) + len(self.lastName)
-        ### find the numbers corresponding
-        yy = nombre[alphabet.index(self.firstName[0].lower())]
-        zz = nombre[alphabet.index(self.lastName[0].lower())]
-
-        return initials + "-"+str(length) + "-" + "%02d-%02d" % (yy,zz)
 
 class transaction(db.Model):
     id = db.Column(db.Integer,nullable=False,primary_key=True)
     amount = db.Column(db.Integer,nullable=False)
     customer_Num = db.Column(db.String,nullable = False)
     account = db.Column(db.String,nullable=False)
+    
 
     def __init__(self, id, amount,customer,account):
         self.id = id
@@ -72,10 +64,6 @@ def getNumAcc(firstName,lastName):
 
         return initials + "-"+str(length) + "-" + "%02d-%02d" % (yy,zz)
 
-
-#database_file = "sqlite:///bank.db"
-
-#app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 
 
@@ -107,9 +95,18 @@ def newCustomer():
     if request.method == 'POST':
         firstName = request.form['customer_firstName'] 
         lastName = request.form['customer_lastName']
-        
+        email = request.form['customer_email']
+
         accountNum = getNumAcc(firstName, lastName)
-        newCos=customer(firstName=firstName,lastName=lastName,accountNumber= accountNum)
+        newCos=customer(firstName=firstName,lastName=lastName,accountNumber= accountNum,email=email)
+        print(os.path.isfile('%s-%s.txt' % (newCos.accountNumber,'savings')))
+        if not os.path.isfile('./customers/%s-%s.txt' % (newCos.accountNumber,'savings')) and not os.path.isfile('./customers/%s-%s.txt' % (newCos.accountNumber,'currents')):
+            f = open('./customers/%s-%s.txt' % (newCos.accountNumber,'savings'),'w')
+            f.write('%s-%s-%s-%s' % (newCos.accountNumber,newCos.firstName,newCos.lastName,'savings'))
+            f.close()
+            f = open('./customers/%s-%s.txt' % (newCos.accountNumber,'currents'),'w')
+            f.write('%s-%s-%s-%s' % (newCos.accountNumber,newCos.firstName,newCos.lastName,'currents'))
+            f.close()
 
         try :
             db.session.add(newCos)
@@ -136,19 +133,31 @@ def delete(accNum):
             if tr.customer_Num == account_to_delete.accountNumber:
                 db.session.delete(tr)
         db.session.commit()
+
+        os.remove('./customers/%s-%s.txt' % (accNum,'currents'))
+        os.remove('./customers/%s-%s.txt' % (accNum,'savings'))
+        
+
         accounts = customer.query.all()
         return render_template("menu_employee.html",accounts= accounts)
     except:
         return "there was a issue"
 
 @app.route("/menu_customer",methods = ['GET','POST'])
-def menuCustomer():
+def menu_customer():
     lastName = request.form['customer_lastName']
-    accountNum = request.form['customer_accountNumber']
+    accNum = request.form['customer_accountNumber']
     PIN = request.form['customer_PIN']
-    account_customer = customer.query.get_or_404(accountNum)
-    if lastName == account_customer.lastName and PIN == account_customer.getPIN():
-        return render_template("menu_customer.html",customer = account_customer)
+    account= customer.query.get_or_404(accNum)
+    currents,savings = [],[]
+    for tr in transaction.query.all():
+        if tr.customer_Num == accNum:
+            if tr.account == "current":
+                currents.append([tr.id,tr.amount])
+            else:
+                savings.append([tr.id,tr.amount])
+    if lastName == account.lastName and PIN == account.getPIN():
+        return render_template("menu_customer.html",customer = account,currents= currents,savings = savings)
     return render_template("log_in_customer.html",message = "not fount try again")
 
 @app.route("/log_out")
@@ -184,6 +193,11 @@ def new_tr(accNum,account):
         amount = request.form['add_tr']
         new_tr = transaction(id, amount, accNum, account)
         cust = customer.query.get_or_404(accNum)
+
+        f = open('./customers/%s-%ss.txt' % (cust.accountNumber,account),'a')
+        f.write('\n%s\t%s\t%s'  % (id,'01-01-01',amount))
+        f.close()
+
         if account =='current':
             cust.currentAccount = cust.currentAccount + int(amount)
         else:
@@ -191,11 +205,13 @@ def new_tr(accNum,account):
         try :
             db.session.add(new_tr)
             db.session.commit()
+
+           
             return redirect("/description/"+accNum)
         except:
-            return "there was an issue"
+            return "there was an issue a"
     else:
-        return "there was a issue"
+        return "there was a issue b"
 
 
 @app.route("/delete_tr/<string:id>",methods=['GET','POST'])
