ebbdd9a1373e1a737b29d6c9553b3d69e99b4055
unknown <cordonnier.antoine@yahoo.fr>
2021-05-08 17:03:41 +0200

    commentary 1

--- bank_main.py
+++ bank_main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 
 
-database_file2 = "sqlite:///transactions.db"
+database_file2 = "sqlite:///bank.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file2
 
 id_tr = 0 
@@ -22,6 +22,9 @@ class customer(db.Model):
     savingAccount = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String,nullable=False)
 
+    #we create a class that contain mains informations for a person and the minimum that we need
+    # we choosed to not take the PIN as a object of the class because it can be taken in the account number.
+
     def __init__(self,firstName,lastName,accountNumber,email):
         
         self.firstName = firstName
@@ -31,20 +34,23 @@ class customer(db.Model):
         self.currentAccount = 0
         self.savingAccount = 0
 
+        #this is the function creating objects of the class
 
     def __repr__(self):
         return '<customer %s>' % self.accountNumber
 
     def getPIN(self):
         return self.accountNumber[-5:-3] + self.accountNumber[-2:]
-
+        #this function allow to get the pin code from the account number by getting the numbers associated
 
 class transaction(db.Model):
     id = db.Column(db.Integer,nullable=False,primary_key=True)
     amount = db.Column(db.Integer,nullable=False)
     customer_Num = db.Column(db.String,nullable = False)
     account = db.Column(db.String,nullable=False)
-    
+    #here is the class of the transactions where it is defined by an id as the primary key, an amount a customer ans the account of the 
+    #customer which will recieved the transaction.  There are all the informations that are needed to get a transactions and each transaction is 
+    #recognizable by its id allowing the costumer to get the same amount on the same account many times.
 
     def __init__(self, id, amount,customer,account):
         self.id = id
@@ -63,7 +69,9 @@ def getNumAcc(firstName,lastName):
         zz = nombre[alphabet.index(lastName[0].lower())]
 
         return initials + "-"+str(length) + "-" + "%02d-%02d" % (yy,zz)
-
+        #this function allows to create the account number of clients based on their firstname and lastname
+        #we create an alphabet to get the place(index) of the initials of names and we concatenate with 
+        # others informations we got easily from names
 
 
 
@@ -71,14 +79,18 @@ def getNumAcc(firstName,lastName):
 @app.route("/", methods = ["GET","POST"])
 def log_in():
     return render_template("log_in.html")
+    #this is the first page we can see and the entry to all the other pages. On this page we will be able to choose if he is a customer 
+    # or a employee
 
 @app.route("/log_in_customer", methods =["GET","POST"] )
 def log_in_customer():
     return render_template("log_in_customer.html")
+    #this the function allowing to go to the log in page for customer
 
 @app.route("/log_in_employee", methods =["GET","POST"] )
 def log_in_employee():
     return render_template("log_in_employee.html")
+    #this code allow also to go to the log in page for employee
 
 @app.route("/menu_employee", methods =["POST"] )
 def menu_employee():
@@ -89,17 +101,17 @@ def menu_employee():
         return render_template("menu_employee.html",accounts= accounts)
     else:
         return render_template ("log_in_employee.html",message = "failed")
-    
+    #this function check if the PIN is correct or not and then dirst you to the menu of employee or back to the log in to retry
+
 @app.route("/new_customer", methods = ["POST"])
 def newCustomer():
     if request.method == 'POST':
         firstName = request.form['customer_firstName'] 
         lastName = request.form['customer_lastName']
         email = request.form['customer_email']
-
-        accountNum = getNumAcc(firstName, lastName)
-        newCos=customer(firstName=firstName,lastName=lastName,accountNumber= accountNum,email=email)
-        print(os.path.isfile('%s-%s.txt' % (newCos.accountNumber,'savings')))
+        #here we recuperate hte informations we will need to add customer like the firstname, the lastname, and the email.
+        accountNum = getNumAcc(firstName, lastName)     #it create the account number with the function created for it
+        newCos=customer(firstName=firstName,lastName=lastName,accountNumber= accountNum,email=email)  #we create the customer wih all informations
         if not os.path.isfile('./customers/%s-%s.txt' % (newCos.accountNumber,'savings')) and not os.path.isfile('./customers/%s-%s.txt' % (newCos.accountNumber,'currents')):
             f = open('./customers/%s-%s.txt' % (newCos.accountNumber,'savings'),'w')
             f.write('%s-%s-%s-%s' % (newCos.accountNumber,newCos.firstName,newCos.lastName,'savings'))
@@ -107,12 +119,15 @@ def newCustomer():
             f = open('./customers/%s-%s.txt' % (newCos.accountNumber,'currents'),'w')
             f.write('%s-%s-%s-%s' % (newCos.accountNumber,newCos.firstName,newCos.lastName,'currents'))
             f.close()
-
+        else:
+            return "there is a problem in the files, some documents are already existing"
+        #we verifiy if the files for the customers are not already taken and then it create it 
         try :
             db.session.add(newCos)
             db.session.commit()
             accounts = customer.query.all()
             return render_template("menu_employee.html",accounts= accounts)
+            #it finaly add the customer in the database and return to the page of the menu of employee
         except:
             return "there was an issue"
     
@@ -122,28 +137,32 @@ def newCustomer():
 @app.route("/new_customerPage",methods  =['GET','POST'])
 def new_customerPage():
     return render_template("new_customer.html")
+    #this function just leed to the page to create a new customer
 
 @app.route("/delete/<string:accNum>",methods = ['GET','POST'])
+#this function will delete en account with all that will go with
 def delete(accNum):
     account_to_delete = customer.query.get_or_404(accNum)
-
+    #we get the customer associated with the account number tranmite by the route(accNum)
     try:
-        db.session.delete(account_to_delete)
+        db.session.delete(account_to_delete) # we delete the count of the customer we have found
         for tr in transaction.query.all():
             if tr.customer_Num == account_to_delete.accountNumber:
-                db.session.delete(tr)
+                db.session.delete(tr) # we deleteall the transactions linked with this account in the database of transactions
         db.session.commit()
 
         os.remove('./customers/%s-%s.txt' % (accNum,'currents'))
         os.remove('./customers/%s-%s.txt' % (accNum,'savings'))
-        
+        #then we delete the files of this account
 
         accounts = customer.query.all()
-        return render_template("menu_employee.html",accounts= accounts)
+        return render_template("menu_employee.html",accounts= accounts) #then we go back to the menu of the employee with the list of all accounts
     except:
         return "there was a issue"
+    
 
 @app.route("/menu_customer",methods = ['GET','POST'])
+#this function will lead to the menu
 def menu_customer():
     lastName = request.form['customer_lastName']
     accNum = request.form['customer_accountNumber']
@@ -170,7 +189,7 @@ def return_menue_employee():
     return render_template("menu_employee.html",accounts= accounts)
 
 @app.route("/description/<string:accNum>",methods = ['GET','POST'])
-def description(accNum):
+def description(accNum,message = ''):
     account= customer.query.get_or_404(accNum)
     currents,savings = [],[]
     for tr in transaction.query.all():
@@ -181,7 +200,7 @@ def description(accNum):
                 savings.append([tr.id,tr.amount])
     
 
-    return render_template("employee_view_customer.html",customer= account,currents= currents,savings = savings)
+    return render_template("employee_view_customer.html",customer= account,currents= currents,savings = savings,message = message)
 
 @app.route("/new_transaction/<string:accNum>/<string:account>",methods = ['GET','POST'])
 def new_tr(accNum,account):
@@ -199,8 +218,12 @@ def new_tr(accNum,account):
         f.close()
 
         if account =='current':
+            if cust.currentAccount + int(amount)< 0:
+                return description(accNum,'failed, your current account cannot be under 0')
             cust.currentAccount = cust.currentAccount + int(amount)
         else:
+            if cust.savingAccount + int(amount)< 0:
+                return description(accNum,'failed, your saving account cannot be under 0')
             cust.savingAccount = cust.savingAccount + int(amount)
         try :
             db.session.add(new_tr)
